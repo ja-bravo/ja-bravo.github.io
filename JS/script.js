2c52e6ce5acd66621dc091a6c7b2583fbaae19c7
Jose Antonio <ja.bravo.isidro@gmail.com>
2015-07-20 21:40:23 +0200

    3.

--- JS/script.js
+++ JS/script.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
     $('#proyect3').click(function(){
     	window.location.href = "http://ja-bravo.github.io/League-of-Legends-stats-viewer/";
     });
+
+    if($(window).width() < 680)
+    {
+        $(".skill:nth-child(1)").css("margin-left","15%");
+        $(".skill:nth-child(3)").css("margin-left","15%");
+        $(".skill:nth-child(5)").css("margin-left","15%");
+    }
+    else
+    {
+        $(".skill:nth-child(1)").css("margin-left","7%");
+        $(".skill:nth-child(4)").css("margin-left","7%");
+    }
 });
